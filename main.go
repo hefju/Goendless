88992c881c5900fb50c9014ef40c3c49a3ff10e8
hefju <hefju@qq.com>
2016-08-11 23:44:13 +0800

    打印变量地址

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ func main() {
 func atest(){
 
     x:=make([]int ,4)
-    fmt.Printf("原地址:%p\n",x)
+    fmt.Printf("原地址:%p\n",x)//打印变量的地址
     x=append(x,5)
     fmt.Printf("后地址:%p\n",x)
     fmt.Println(x)
